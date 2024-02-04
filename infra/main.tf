0a7f559047383155c00b57d21abee29a4e26d1ac
Brian Sheridan <brian.sheridan@gmail.com>
2024-02-04 13:43:04 -0800

    TF

---
+++ infra/main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {}
+
+locals {
+  location = "westus2"
+  resource_group_name = "rg-webappcontainer"
+  app_service_plan_name = "asp-webappcontainer"
+  app_service_name = "app-webappcontainer"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = local.resource_group_name
+  location = local.location
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  location            = local.location
+  name                = local.app_service_plan_name
+  resource_group_name = local.resource_group_name
+}
